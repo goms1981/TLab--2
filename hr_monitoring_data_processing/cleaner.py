e2082e58f48e580269af3a33360e91f2cf49397e
Gomathy Ramachandran <gomathy.ramachandrann@gmail.com>
2024-12-04 21:38:08 -0500

    Tlab 2

---
+++ hr_monitoring_data_processing/cleaner.py
@@ -0,0 +1,31 @@
+def filter_nondigits(data: list) -> list:
+    """
+    Filter all strings from list that are not integers
+
+    Args:
+        data (list[str]): list of strings representing heart rate samples.
+            Might contain invalid or missing data.
+    Returns:
+        list[int]: list of integers, with all non-digit strings removed
+    """
+    new_list = []
+
+    for item in data:
+        x = item.strip()
+
+        if x.isdigit():
+            new_list.append(int(x))
+        else:
+            continue
+    return new_list
+
+
+
+def filter_outliers(data: list) -> list:
+    hrt =[]
+    for item in data:
+        if item >30 and item < 250:
+            hrt.append(item)
+        else:
+            continue
+    return hrt    
